d963c69344e64daca48c63162f0e02a03d4280aa
Fredrick Gough <vf15810@umbc.edu>
2021-10-18 13:00:14 -0400

    Basic Skeleton for Login Page with no functionality yet (Now with a test).

--- loginPage/tests.py
+++ loginPage/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+import unittest
+from django.shortcuts import render
+
+#Tests if the pages exist
+class URLTests(unittest.TestCase):
+    def testGetLoginPage(self):
+        request = 'index'
+        self.assertTrue(render(request, 'loginPage/index.html', context={}))
