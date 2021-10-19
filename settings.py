2b004aef4efa8cc58ac28d2e77ca7ae89bf02d0a
william-c-fenton <wcfenton12@gmail.com>
2021-10-19 16:22:32 -0400

    Removed secret key :) and moved asgi.py, settings.py, urls.py, wsgi.py out of userPage and into base directory

--- settings.py
+++ settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3*z6&&7(hy!#ap#s^o+lw#b8-ya&wek!n!7(3ikr_!@=i177#%'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
