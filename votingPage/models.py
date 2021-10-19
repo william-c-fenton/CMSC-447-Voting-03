103e9fef6d82e4bda84616a3ed5b706422c99b4c
william-c-fenton <wcfenton12@gmail.com>
2021-10-19 16:12:30 -0400

    Laid out basic database model for the votes system in votingPage\models.py

--- votingPage/models.py
+++ votingPage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from ./loginPage import models.py
 
 # Create your models here.
+class Ballot(models.Model):
+    option = models.CharField(max_length=50)
+
+class Votes(models.Model):
+    voter = models.ForeignKey(VoterInfo, on_delete=models.CASCADE)
+    vote = models.ForeignKey(Ballot, on_delete=models.CASCADE)
+    
