c4754db87df2485bd6b8af092fa04f24e483db22
william-c-fenton <wcfenton12@gmail.com>
2021-10-20 17:50:51 -0400

    Fixed Django configuration and some import statements.

--- settings.py
+++ settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'userPage.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'userPage.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
